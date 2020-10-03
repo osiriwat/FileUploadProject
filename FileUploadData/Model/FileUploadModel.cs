f6467b0a72b1c861051cd372d5a0eeaeef014415
osiriwat <osiriwat@gmail.com>
2020-10-03 11:52:29 +0700

    Commit model

---
+++ FileUploadData/Model/FileUploadModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FileUploadData.Model
+{
+    public class FileUploadModel
+    {
+        public string Id { get; set; }
+        public string Payment { get; set; }
+        public string Status { get; set; }
+    }
+}
