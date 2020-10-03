19f63d5187d50f5bbe384a9966d868dfac4821d8
osiriwat <osiriwat@gmail.com>
2020-10-03 12:07:01 +0700

    Create service fil upload on bisiness logig layer

---
+++ FileUploadBusiness/IFileUploadService.cs
@@ -0,0 +1,18 @@
+﻿using FileUploadData;
+using FileUploadData.Model;
+using System;
+using System.Collections.Generic;
+
+namespace FileUploadBusiness
+{
+    public interface IFileUploadService
+    {
+        bool insert(IEnumerable<FileUpload> fileuploads);
+
+        IEnumerable<FileUploadModel> getByCurrency(string currencyCode);
+
+        IEnumerable<FileUploadModel> getByDateRange(DateTime startTranDate, DateTime endTranDate);
+
+        IEnumerable<FileUploadModel> getByStatus(string status);
+    }
+}
